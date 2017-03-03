ed63265cbca8b954e2c960d49704affe0b3a2a7c
jpsfitz <jpsfitz@users.noreply.github.com>
2017-03-03 11:01:57 +0100

    Mie + analysis, core-shell NPs
    Analysis and Mie theory simulation of core-shell NPs with small Au cores and thermally-responsive PNIPAM shells.

--- header_parallel.py
+++ header_parallel.py
@@ -15,7 +15,16 @@ print("   Cluster Direct View variable \'cluster\'")
 nKernels = len(kernels.ids)
 print("   Variable \'nKernels\' =",nKernels)
 
+# change cluster current working directory
+def f(cwd): os.chdir(cwd); print(os.getcwd); return
+cwd = os.getcwd()
+cwdList = []
+for i in range(nKernels): cwdList.append(cwd)
+with cluster.sync_imports(): import os
+cluster.map_sync(f, cwdList)
+
 # initialize parallel kernels
-with cluster.sync_imports():
-    import mie
-    import mie_local
+
+#with cluster.sync_imports():
+#    import mie
+#    import refractive_index_library as ri
